268bcde0b12d46d6d898046607245d3c9e786ae6
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-22 15:59:41 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/MaintenanceLog.java
+++ backend/src/main/java/com/example/demo/entity/MaintenanceLog.java
@@ -4,12 +4,18 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = )
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "maintenance_logs")
 public class MaintenanceLog
 {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "asset_id",nullable = false)
+    private Ind
 }
