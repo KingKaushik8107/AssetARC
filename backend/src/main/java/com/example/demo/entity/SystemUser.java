1f033ab34953571623d48a0e3ab9c7b122446cea
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-22 08:49:02 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/SystemUser.java
+++ backend/src/main/java/com/example/demo/entity/SystemUser.java
@@ -19,5 +19,35 @@ public class SystemUser
     private String password;
 
     @Enumerated(EnumType.STRING)
-    private enum role;
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    
 }
