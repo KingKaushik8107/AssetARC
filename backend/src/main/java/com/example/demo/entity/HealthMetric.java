4b5aebda6dd6bab0abfae3d2dc0148358934fa4c
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-22 15:50:41 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/HealthMetric.java
+++ backend/src/main/java/com/example/demo/entity/HealthMetric.java
@@ -1,9 +1,6 @@
 package com.example.demo.entity;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-import com.example.demo.enums.AssetStatus;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.*;
 import lombok.*;
@@ -19,31 +16,9 @@ public class HealthMetric
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+ 
+    private int asset;
 
-    @Column(name = "asset_tag",unique = true,nullable = false)
-    private String assetTag;
-
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false)
-    private String category;
-
-    @Column(name = "install_date", nullable = false)
-    private LocalDate installDate;
-
-    @Column(name = "purchase_price",nullable = false)
-    private BigDecimal purchasePrice;
-
-    @Column(name = "expected_lifespan_years",nullable = false)
-    private int expectedLifespanYears;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "current_status",nullable = false)
-    private AssetStatus currentStatus;
-
-    @Column(name = "current_health")
-    private int currentHealth;
-
+    private LocalDateTime
 
 }
