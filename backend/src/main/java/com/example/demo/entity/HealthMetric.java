ade79c551d3d8bb5b6080fd356aaae32ee1c6ab5
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-22 09:44:36 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/HealthMetric.java
+++ backend/src/main/java/com/example/demo/entity/HealthMetric.java
@@ -18,6 +18,6 @@ public class HealthMetric
     @Column(name = "asset_tag",unique = true,nullable = false)
     private String assetTag;
 
-    @Column(name = )
+    @Column(n)
     private String name;
 }
