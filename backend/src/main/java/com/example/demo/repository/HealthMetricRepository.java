1f675c9b447dce15dd05dad3bafab39f4d3b3204
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-25 11:11:10 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/HealthMetricRepository.java
+++ backend/src/main/java/com/example/demo/repository/HealthMetricRepository.java
@@ -1,8 +1,15 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
 
-public interface HealthMetricRepository extends JpaRepository<>
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.HealthMetric;
+
+@Repository
+public interface HealthMetricRepository extends JpaRepository<HealthMetric,Long>
 {
+    List<HealthMetric>findByAssetIdOrderByRecordedAtDesc(Long assetId);
     
 }
