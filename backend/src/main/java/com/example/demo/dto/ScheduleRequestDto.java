9125cfc400b71ea80ff09df330819f76ebc7fc54
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-25 12:29:11 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/ScheduleRequestDto.java
+++ backend/src/main/java/com/example/demo/dto/ScheduleRequestDto.java
@@ -3,11 +3,17 @@ package com.example.demo.dto;
 import java.time.LocalDate;
 
 import com.example.demo.enums.MaintenanceType;
+import com.example.demo.enums.Priority;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ScheduleRequestDto
 {
     private Long assetId;
     private LocalDate plannedDate;
     private MaintenanceType maintenanceType;
-    private Prio priority;
+    private Priority priority;
 }
