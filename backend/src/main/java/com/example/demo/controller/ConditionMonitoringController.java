52a90acc1bfcde4e7f84f0ad3873fb33de64feba
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-25 11:24:40 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/ConditionMonitoringController.java
+++ backend/src/main/java/com/example/demo/controller/ConditionMonitoringController.java
@@ -1,17 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.dto.HealthMetricDto;
+import com.example.demo.service.ConditionMonitoringService;
 
 @RestController
 @RequestMapping("/api/monitoring")
 public class ConditionMonitoringController
 {
+    @Autowired
+    ConditionMonitoringService service;
+
     @PostMapping("/metrics")
-    public ResponseEntity<> createMetrics(@RequestBody HealthMetricDto healthMetricDto)
+    public ResponseEntity<> createMetrics(@RequestBody HealthMetricDto dto)
     {
-        
+        service.recordHealthMetric(dto);
     }
 }
