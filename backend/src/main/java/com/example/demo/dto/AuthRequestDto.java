14d4f74ef649e7d408eb09755a973fd48a579077
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-25 12:18:41 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/AuthRequestDto.java
+++ backend/src/main/java/com/example/demo/dto/AuthRequestDto.java
@@ -1,8 +1,5 @@
 package com.example.demo.dto;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
 import lombok.*;
 
 @Data
@@ -11,11 +8,6 @@ import lombok.*;
 @AllArgsConstructor
 public class AuthRequestDto
 {
-    private String token;
-    private Long id;
     private String username;
-    private String installDate;
-    private BigDecimal purchaseprice;
-    private Integer expectedLifespanYears;
-
+    private String password;
 }
--- backend/src/main/java/com/example/demo/dto/AuthResponseDto.java
+++ backend/src/main/java/com/example/demo/dto/AuthResponseDto.java
@@ -1,5 +1,9 @@
 package com.example.demo.dto;
 
-public class AuthResponseDto {
-    
+public class AuthResponseDto
+{
+    private String token;
+    private Long id;
+    private String username;
+    private String role;    
 }
