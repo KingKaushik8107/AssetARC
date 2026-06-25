49c6d15b4b59191e8430ba0000f7bd872660dcae
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-25 11:06:40 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
+++ backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.SystemUser;
 
-public interface SystemUserRepository extends JpaRepository<SystemUser,>
+@Repository
+public interface SystemUserRepository extends JpaRepository<SystemUser,Long>
 {
 
     
