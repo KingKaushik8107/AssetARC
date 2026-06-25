b272fe9bb65655fb493b10bb8fbeb9250f97cd8e
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-25 12:35:11 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/MaintenanceLogRepository.java
+++ backend/src/main/java/com/example/demo/repository/MaintenanceLogRepository.java
@@ -1,5 +1,15 @@
 package com.example.demo.repository;
 
-public class MaintenanceLogRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.MaintenanceLog;
+
+@Repository
+public interface MaintenanceLogRepository extends JpaRepository<MaintenanceLog,Long>
+{
+    List<MaintenanceLog> findByAssetId(Long assetId);
     
 }
