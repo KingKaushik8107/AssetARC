d39e5137ecc05c5998d6f010c7f2360872bb80ae
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-25 12:15:41 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/AuthRequestDto.java
+++ backend/src/main/java/com/example/demo/dto/AuthRequestDto.java
@@ -1,5 +1,14 @@
 package com.example.demo.dto;
 
-public class AuthRequestDto {
-    
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class AuthRequestDto
+{
+    private String assetTag;
+    private String name;
+    private String category;
+    private LocalDate installDate;
+    private BigDecimal purchaseprice;
+    private
 }
