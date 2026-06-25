69d1882870565fa6f4d58f10ee4646a80dc69d3c
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-25 11:26:10 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/ConditionMonitoringService.java
+++ backend/src/main/java/com/example/demo/service/ConditionMonitoringService.java
@@ -1,5 +1,11 @@
 package com.example.demo.service;
 
-public class ConditionMonitoringService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConditionMonitoringService
+{
+    @Autowired
     
 }
