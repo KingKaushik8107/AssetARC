f1454c9177f3fbf5e13cc91b1f943b7f2ea385dd
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-25 12:24:41 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/RegisterDto.java
+++ backend/src/main/java/com/example/demo/dto/RegisterDto.java
@@ -1,8 +1,16 @@
 package com.example.demo.dto;
 
+import com.example.demo.enums.Role;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterDto
 {
     private String username;
     private String password;
-    
+    private Role role;
 }
