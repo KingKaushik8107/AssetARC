3d76fe2daa48032b511491e6d03921ec68317eaa
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-25 12:20:11 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/AuthResponseDto.java
+++ backend/src/main/java/com/example/demo/dto/AuthResponseDto.java
@@ -1,5 +1,11 @@
 package com.example.demo.dto;
 
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponseDto
 {
     private String token;
--- backend/src/main/java/com/example/demo/dto/DashboardStatsDto.java
+++ backend/src/main/java/com/example/demo/dto/DashboardStatsDto.java
@@ -1,5 +1,12 @@
 package com.example.demo.dto;
 
-public class DashboardStatsDto {
+import java.math.BigDecimal;
+
+public class DashboardStatsDto
+{
+    private Long totalAssets;
+    private Long activeMaintenanceCount;
+    private Double averageHealthScore;
+    private BigDecimal totalFleetValue;
     
 }
