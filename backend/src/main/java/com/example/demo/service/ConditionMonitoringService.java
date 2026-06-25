dbc6d7de64479697661308d129f0f948d3eb9102
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-25 11:27:40 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/ConditionMonitoringService.java
+++ backend/src/main/java/com/example/demo/service/ConditionMonitoringService.java
@@ -3,9 +3,17 @@ package com.example.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.HealthMetricDto;
+import com.example.demo.repository.HealthMetricRepository;
+
 @Service
 public class ConditionMonitoringService
 {
     @Autowired
-    
+    HealthMetricRepository repository;
+
+    public void recordHealthMetric(HealthMetricDto dto)
+    {
+        
+    }
 }
