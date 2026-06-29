c39ccf531ed563491fe4992d616b9f2c0fe29e9f
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 21:38:25 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/DashboardService.java
+++ backend/src/main/java/com/example/demo/service/DashboardService.java
@@ -1,5 +1,19 @@
 package com.example.demo.service;
 
-public class DashboardService {
-    
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dto.DashboardStatsDto;
+import com.example.demo.entity.IndustrialAsset;
+import com.example.demo.repository.IndustrialAssetRepository;
+
+@Service
+public class DashboardService
+{
+    @Autowired
+    IndustrialAssetRepository assetRepository;
+
+    @Transactional(readOnly = true)
+    public DashboardStatsDto gDashboardStatsDto
 }
