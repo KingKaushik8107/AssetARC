3589ae6a57240597461f20cb22bb0404e0c941d5
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 16:23:06 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/MaintenanceController.java
+++ backend/src/main/java/com/example/demo/controller/MaintenanceController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.demo.entity.MaintenanceSchedule;
 import com.example.demo.service.MaintenanceService;
 
 @RestController
@@ -13,5 +17,8 @@ public class MaintenanceController
     MaintenanceService service;
 
     @GetMapping("/schedules")
-    
+    public ResponseEntity<List<MaintenanceSchedule>> getSchedules()
+    {
+        service.getUpcomingSchedules();
+    }
 }
