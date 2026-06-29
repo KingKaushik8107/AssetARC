a71bbdcd5862b97dba7746cf6511d840ef0f950e
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 22:05:46 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/AuthService.java
+++ backend/src/main/java/com/example/demo/service/AuthService.java
@@ -1,9 +1,22 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.SystemUserRepository;
+import com.example.demo.security.JwtService;
 
 @Service
 public class AuthService
 {
-    
+    @Autowired
+    SystemUserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autow
 }
