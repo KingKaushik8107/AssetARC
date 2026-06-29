4e7ae0a96a6013ae810928922a08e6fe719f0788
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 21:39:55 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/DashboardService.java
+++ backend/src/main/java/com/example/demo/service/DashboardService.java
@@ -1,11 +1,15 @@
 package com.example.demo.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.dto.DashboardStatsDto;
 import com.example.demo.entity.IndustrialAsset;
+import com.example.demo.enums.AssetStatus;
 import com.example.demo.repository.IndustrialAssetRepository;
 
 @Service
@@ -15,5 +19,14 @@ public class DashboardService
     IndustrialAssetRepository assetRepository;
 
     @Transactional(readOnly = true)
-    public DashboardStatsDto gDashboardStatsDto
+    public DashboardStatsDto gDashboardStatsDto()
+    {
+        long totalAssets = assetRepository.count();
+        long activeMaintenance = assetRepository.countByCurrentStatus(AssetStatus.UNDER_MAINTENANCE);
+
+        List<IndustrialAsset> assets = assetRepository.findAll();
+
+        BigDecimal totalValue = assets.stream()
+            .map(In)
+    }
 }
