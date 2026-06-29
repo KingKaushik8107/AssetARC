6a8d661bd1e81b0e4874176a408c0808c3a9c710
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 13:58:13 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AssetController.java
+++ backend/src/main/java/com/example/demo/controller/AssetController.java
@@ -58,6 +58,7 @@ public class AssetController
     // @PreAuthorize("hasAnyRole('ASSET_MANAGER','SYSTEM_ADMIN')")
     public ResponseEntity<String> deleteAsset(@PathVariable Long id)
     {
-        service.
+        service.decommisionAsset(id);
+        return ResponseEntity.ok("Asset deleted successfully");
     }
 }
