a9983a370bd7a3bf70b13b9d27d849efe32e99db
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 11:57:56 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/MaintenanceScheduleRepository.java
+++ backend/src/main/java/com/example/demo/repository/MaintenanceScheduleRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.entity.MaintenanceSchedule;
+import com.example.demo.enums.ScheduleStatus;
+
 @Repository
-public interface MaintenanceScheduleRepository 
+public interface MaintenanceScheduleRepository extends JpaRepository<MaintenanceSchedule,Long>
 {
-    
+    List<MaintenanceSchedule>findByAssetIdAndStatus(Long assetId,ScheduleStatus status);
+    List<MaintenanceSchedule>findByStatus
 }
