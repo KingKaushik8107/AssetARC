eff655afbfb17d71c7f1dee1fea05a6d25e01ef9
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 21:35:25 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/DashboardController.java
+++ backend/src/main/java/com/example/demo/controller/DashboardController.java
@@ -1,10 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.service.DashboardService;
 
 @RestController
 @RequestMapping("/api/dashboard")
 public class DashboardController
 {
-    
+    @Autowired
+    DashboardService service;
+
+    @GetMapping("/stats")
+    @PreAuthorize("hasAnyRole('ASSET_MANAGER','OPERATIONS_SUPERVISOR','')")
 }
