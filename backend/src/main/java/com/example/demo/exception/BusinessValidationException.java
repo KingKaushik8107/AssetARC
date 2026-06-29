d7df38682e2875a2bf0a3c1905e30d8690733901
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 20:20:25 +0530

    User commit

--- backend/src/main/java/com/example/demo/exception/BusinessValidationException.java
+++ backend/src/main/java/com/example/demo/exception/BusinessValidationException.java
@@ -1,5 +1,10 @@
 package com.example.demo.exception;
 
-public class BusinessValidationException {
+public class BusinessValidationException extends RuntimeException
+{
+    public BusinessValidationException()
+    {
+        super();
+    }
     
 }
