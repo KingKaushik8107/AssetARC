2a089a0e4ab2e4051f4c7ed0afbd8690692b3894
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 21:30:55 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/HealthController.java
+++ backend/src/main/java/com/example/demo/controller/HealthController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.ConditionMonitoringService;
 
 @RestController
 @RequestMapping("/api/health")
 public class HealthController
 {
     @Autowired
-    Healt
+    ConditionMonitoringService monitoringService;
+
+    @PostMapping("/record")
+    // @PreAuthorize("hasAnyRole('MAINTENANCE_TECHNICIAN')")
+    
 }
