add931e4b1196c8e182495d9a978cf7d6843471c
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 11:50:26 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/IndustrialAssetRepository.java
+++ backend/src/main/java/com/example/demo/repository/IndustrialAssetRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.entity.IndustrialAsset;
+
 @Repository
-public interface IndustrialAssetRepository extends JpaRepository<Indus
+public interface IndustrialAssetRepository extends JpaRepository<IndustrialAsset,Long>
 {
-    
+    Optional<IndustrialAsset>findByAssetTag(String assetTag)
 }
