c6532cf8574578142be2c29959dc4db3b55f3c9f
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 16:29:06 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/MaintenanceService.java
+++ backend/src/main/java/com/example/demo/service/MaintenanceService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.ScheduleRequestDto;
 import com.example.demo.entity.MaintenanceLog;
 import com.example.demo.entity.MaintenanceSchedule;
 import com.example.demo.enums.ScheduleStatus;
@@ -23,5 +24,13 @@ public class MaintenanceService
         return scheduleRepository.findByStatus(ScheduleStatus.PENDING);
     }
 
-    public List<MaintenanceLog> getAllLogs
+    public List<MaintenanceLog> getAllLogs()
+    {
+        return logRepository.findAll();
+    }
+
+    public MaintenanceSchedule scheduleMaintenance(ScheduleRequestDto dto)
+    {
+        
+    }
 }
