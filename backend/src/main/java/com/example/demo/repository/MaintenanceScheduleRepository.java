bf2894035a435ab95446655340090f2774a114a2
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 11:58:59 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/MaintenanceScheduleRepository.java
+++ backend/src/main/java/com/example/demo/repository/MaintenanceScheduleRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.MaintenanceSchedule;
@@ -12,5 +13,8 @@ import com.example.demo.enums.ScheduleStatus;
 public interface MaintenanceScheduleRepository extends JpaRepository<MaintenanceSchedule,Long>
 {
     List<MaintenanceSchedule>findByAssetIdAndStatus(Long assetId,ScheduleStatus status);
-    List<MaintenanceSchedule>findByStatus
+    List<MaintenanceSchedule>findByStatus(ScheduleStatus status);
+
+    @Query("SELECT s FROM MaintenanceSchedule s WHERE s.status = 'PENDING'")
+    List<MaintenanceSchedule>findPendingSchedules();
 }
