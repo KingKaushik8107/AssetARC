ea4e46d0fbb14033ab574dd5e837318443f654d4
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 22:20:47 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/AuthService.java
+++ backend/src/main/java/com/example/demo/service/AuthService.java
@@ -3,6 +3,10 @@ package com.example.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import com.example.demo.dto.AuthRequestDto;
 import com.example.demo.dto.AuthResponseDto;
 import com.example.demo.dto.RegisterDto;
@@ -50,7 +54,7 @@ public class AuthService
     {
         authenticationManager.authenticate(
             new UsernamePasswordAuthenticationToken(
-                dto.getUsername()
+                dto.getUsername(),
                 dto.getPassword()
             )
         );
