78f2bd688d27163dadfee4654550bb9ef293ea8d
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 16:26:06 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/MaintenanceService.java
+++ backend/src/main/java/com/example/demo/service/MaintenanceService.java
@@ -2,12 +2,22 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.MaintenanceSchedule;
+import com.example.demo.repository.MaintenanceLogRepository;
+import com.example.demo.repository.MaintenanceScheduleRepository;
 
 @Service
 public class MaintenanceService
 {
-    public List<MaintenanceSchedule> getUpcomingSchedules
+    @Autowired
+    MaintenanceScheduleRepository scheduleRepository;
+    MaintenanceLogRepository maintenanceLogRepository;
+
+    public List<MaintenanceSchedule> getUpcomingSchedules()
+    {
+        return 
+    }
 }
