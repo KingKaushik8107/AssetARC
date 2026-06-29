3a339c8ae53538c74fff342dfbfdcf9f49a13b13
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 21:23:25 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/HealthController.java
+++ backend/src/main/java/com/example/demo/controller/HealthController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/health")
 public class HealthController
 {
-    
+    @Autowired
+    Healt
 }
