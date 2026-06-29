dd935a5f59a6db95dba60686a3fd9992b3fe5116
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 22:01:17 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,11 +1,19 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.dto.AuthResponseDto;
+import com.example.demo.service.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController
 {
-    
+    @Autowired
+    AuthService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDto> register(@RequestBody Regi)
 }
