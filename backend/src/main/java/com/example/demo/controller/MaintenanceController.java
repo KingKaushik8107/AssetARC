3b9cd1f446880ca15fbbfa23fca1f9d49d8342cd
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 16:21:36 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/MaintenanceController.java
+++ backend/src/main/java/com/example/demo/controller/MaintenanceController.java
@@ -1,10 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.service.MaintenanceService;
 
 @RestController
 @RequestMapping("/api/maintenance")
 public class MaintenanceController
 {
+    @Autowired
+    MaintenanceService service;
+
+    @GetMapping("/schedules")
     
 }
