ac4f622d5ac756a2ea64aec8d6b7e560dee5fef7
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-29 12:02:26 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AssetController.java
+++ backend/src/main/java/com/example/demo/controller/AssetController.java
@@ -1,10 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.service.AssetService;
 
 @RestController
 @RequestMapping("/api/assets")
 public class AssetController
 {
-    
+    @Autowired
+    AssetService service;
+
+    @PostMapping("")
 }
