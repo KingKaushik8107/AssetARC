29bdc63cab8b22fb539d96a08ecc712766d865a3
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-06-30 09:11:09 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/AssetService.java
+++ backend/src/main/java/com/example/demo/service/AssetService.java
@@ -104,6 +104,6 @@ public class AssetService
             .orElseThrow( ()-> new ResourceNotFoundException("Asset not found"));
            
         asset.setCurrentStatus(AssetStatus.DECOMMISSIONED);
-        repo.save(asset);
+        // repo.save(asset);
     }
 }
