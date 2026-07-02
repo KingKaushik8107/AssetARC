dc3ce93fcfeb308d4ee79780e3938fb05cdbb87e
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-07-02 13:17:52 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/AssetService.java
+++ backend/src/main/java/com/example/demo/service/AssetService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.dto.AssetRequestDto;
 import com.example.demo.dto.DashboardStatsDto;
@@ -18,7 +19,7 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.IndustrialAssetRepository;
 import com.example.demo.repository.MaintenanceScheduleRepository;
 
-import jakarta.transaction.Transactional;
+
 import lombok.*;
 
 @Service
