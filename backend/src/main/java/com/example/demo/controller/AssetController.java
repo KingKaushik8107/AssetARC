3385d7adc10fe8afd24dcffde714e2b172abae7c
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-07-02 10:30:08 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AssetController.java
+++ backend/src/main/java/com/example/demo/controller/AssetController.java
@@ -1,29 +1,24 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.dto.AssetRequestDto;
 import com.example.demo.dto.DashboardStatsDto;
 import com.example.demo.entity.IndustrialAsset;
 import com.example.demo.service.AssetService;
 
+import lombok.*;
+
 @RestController
 @RequestMapping("/api/assets")
+@RequiredArgsConstructor
 public class AssetController
 {
     private final AssetService service;
-
-    @Autowired
-    public AssetController(AssetService service)
-    {
-        this.service = service;
-    }
 
     @GetMapping("/stats")
     public ResponseEntity<DashboardStatsDto> getStats()
@@ -32,7 +27,7 @@ public class AssetController
     }
 
     @GetMapping
-    public ResponseEntity<Page<IndustrialAsset>>getAllAssets(Pageable pageable)
+    public ResponseEntity<Page<IndustrialAsset>> getAllAssets(Pageable pageable)
     {
         return ResponseEntity.ok(service.getAllAssets(pageable));
     }
@@ -44,25 +39,24 @@ public class AssetController
     }
 
     @PostMapping
-    @PreAuthorize("hasAnyRole('ASSET_MANAGER','SYSTEM_ADMIN')")
+    @PreAuthorize("hasAnyRole('ASSET_MANAGER', 'SYSTEM_ADMIN')")
     public ResponseEntity<String> createAsset(@RequestBody AssetRequestDto dto)
     {
         service.createAsset(dto);
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body("Asset created successfully.");
+        return ResponseEntity.status(201).body("Asset created successfully.");
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ASSET_MANAGER','SYSTEM_ADMIN')")
+    @PreAuthorize("hasAnyRole('ASSET_MANAGER', 'SYSTEM_ADMIN')")
     public ResponseEntity<String> updateAsset(@PathVariable Long id, @RequestBody AssetRequestDto dto)
     {
         service.updateAsset(id, dto);
         return ResponseEntity.ok("Asset updated successfully.");
     }
-    
+
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ASSET_MANAGER','SYSTEM_ADMIN')")
-    public ResponseEntity<String> deleteAsset(@PathVariable Long id)
+    @PreAuthorize("hasAnyRole('ASSET_MANAGER', 'SYSTEM_ADMIN')")
+    public ResponseEntity<String> decommissionAsset(@PathVariable Long id)
     {
         service.decommissionAsset(id);
         return ResponseEntity.ok("Asset deleted successfully.");
