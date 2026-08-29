93106526c44a890a275c65a68cafcf43a4b10e3e
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-08-29 10:36:15 +0530

    User commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  // baseURL: 'http://localhost:8080/api'
+  baseURL: 'http://172.100.142.164:8080/api'
 });
 
 api.interceptors.request.use((config) => {
