8b7272c7759acb24237fea25f930e7d3532d179e
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-08-31 14:55:17 +0530

    User commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,26 +1,42 @@
 import axios from 'axios';
 
+const BASE_URL =
+  process.env.REACT_APP_API_URL || 'http://localhost:8080';
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
-});
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}) || axios;
 
-api.interceptors.request.use((config) => {
-  const user = JSON.parse(localStorage.getItem('user'));
-  if (user && user.token) {
-    config.headers.Authorization = `Bearer ${user.token}`;
-  }
-  return config;
-});
+// Request interceptor
+if (api.interceptors && api.interceptors.request) {
+  api.interceptors.request.use((config) => {
+    const user = JSON.parse(localStorage.getItem('user'));
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem('user');
-      window.location.href = '/login';
+    if (user && user.token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${user.token}`;
     }
-    return Promise.reject(error);
-  }
-);
+
+    return config;
+  });
+}
+
+// Response interceptor
+if (api.interceptors && api.interceptors.response) {
+  api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response && error.response.status === 401) {
+        localStorage.removeItem('user');
+        window.location.href = '/login';
+      }
+
+      return Promise.reject(error);
+    }
+  );
+}
 
 export default api;
