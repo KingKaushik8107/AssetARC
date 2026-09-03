0035d30f65d23e43aac5f0e17e8bc3d631697bba
KAUSHIK K K <727824TUAD102@skct.edu.in>
2026-09-03 08:38:22 +0530

    User commit

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -62,11 +62,8 @@ const Navbar = () => {
       <div className="navbar-user">
         <ThemeToggle />
         <div className="user-profile-badge">
-          <div className="user-avatar">{user.username.charAt(0).toUpperCase()}</div>
-          {/* <div className="user-details">
-            <span className="user-name">Welcome, {user.username}</span>
-            <span className="user-role-tag">{user.role ? user.role.replace('_', ' ') : 'USER'}</span>
-          </div> */}
+          <div className="user-avatar">{(user?.username || user?.name || 'U').charAt(0).toUpperCase()}</div>
+         
           <div className="user-details">
             <span className="user-name">
               Welcome, {user.username}
