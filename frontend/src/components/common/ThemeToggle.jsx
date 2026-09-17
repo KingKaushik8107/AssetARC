845909dfd0f954824ecf328628960a21ec12a569
KAUSHIK K K <kaushikkanagu07@gmail.com>
2026-09-17 09:07:39 +0530

    Edited

--- frontend/src/components/common/ThemeToggle.jsx
+++ frontend/src/components/common/ThemeToggle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTheme } from '../../context/ThemeContext';
 
 const ThemeToggle = ({ showLabel = false, className = '' }) => {
-  const { theme, toggleTheme, isDark } = useTheme();
+  const { toggleTheme, isDark } = useTheme();
 
   return (
     <button
